80bfe461ba0969d3fcd55f820a0a9ff8efb71bea
RC <rchanou@gmail.com>
2018-01-14 08:42:38 -0800

    chore: drastically simplify proof-of-concept new function generation system, remove immer
    sorry immer, you helped but you're no longer needed

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-const produce = require("immer").default;
-
 const base = {
   0: [{ o: "." }, { o: "g" }, "Math"],
   1: [{ o: "." }, { r: 0 }, "pow"],
-  2: [{ r: 1 }, 5, 2],
-  3: [{ r: 1 }, 12, 2],
+  20: 5,
+  21: 12,
+  2: [{ r: 1 }, { r: 20 }, 2],
+  3: [{ r: 1 }, { r: 21 }, 2],
   4: [{ o: "." }, { r: 0 }, "sqrt"],
   5: [{ o: "+" }, { r: 2 }, { r: 3 }],
   6: [{ r: 4 }, { r: 5 }]
 };
 
 const calc = (repo, id) => {
-  const nodes = repo[id];
-  const [func, ...args] = nodes.map(node => {
+  const calcNode = node => {
     if (typeof node !== "object") {
       return node;
     }
@@ -31,60 +30,41 @@ const calc = (repo, id) => {
       default:
         return calc(repo, node.r);
     }
-  });
-  return func(...args);
+  };
+
+  const nodes = repo[id];
+
+  if (!Array.isArray(nodes)) {
+    return calcNode(nodes);
+  }
+
+  const [funcNode, ...argNodes] = nodes;
+  const funcVal = calcNode(funcNode);
+
+  if (typeof funcVal !== "function") {
+    // TODO: could this possibly have different behavior?
+    return funcVal;
+  }
+
+  const argVals = argNodes.map(calcNode);
+  return funcVal(...argVals);
+};
+
+const reify = (repo, id, ...argIds) => {
+  return (...args) => {
+    for (let i = 0; i < argIds.length; i++) {
+      repo[argIds[i]] = args[i];
+    }
+
+    return calc(repo, id);
+  };
 };
 
 const a = calc(base, 6);
 console.log(a, global.Math.pow(5, 2));
 
-const reify = (repo, id, ...argSlots) => {
-  return (...args) => {
-    const modded = produce(repo, draft => {
-      for (let i = 0; i < argSlots.length; i++) {
-        const [slotId, slotIndex] = argSlots[i];
-        draft[slotId][slotIndex] = args[i];
-      }
-    });
-    return calc(modded, id);
-  };
-
-  // const argIndices = [];
-
-  // const subReify = id => {
-  //   if (argSlots.some(([slotId]) => slotId == id)) {
-  //     const [head, ...tail] = test[id];
-  //     const headVal = calcNode(head);
-  //     const tailVals = [];
-  //     for (let i = 0; i < tail.length; i++) {
-  //       if (argSlots.some(([slotId, j]) => slotId == id && j - 1 == i)) {
-  //         argIndices.push(i);
-  //       } else {
-  //         const node = tail[i];
-  //         if (typeof node === "object" && node.r) {
-  //           const wut = subReify(node.r);
-  //         } else {
-  //           tailVals[i] = calcNode(tail[i]);
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return calc(id);
-  // };
-
-  // return (...args) => {
-  //   const vals = [...tailVals];
-  //   for (let i = 0; i < args.length; i++) {
-  //     vals[argIndices[i]] = args[i];
-  //   }
-
-  //   return headVal(...vals);
-  // };
-};
-
-const b = reify(base, 2, [2, 1]);
+const b = reify(base, 2, 20);
 console.log(b(12), Math.pow(12, 2));
 
-const d = reify(base, 6, [2, 1], [3, 1]);
-console.log("d", d(11, 60));
+const d = reify(base, 6, 20, 21);
+console.log("d", d(11, 60), d(7, 24, "hullaballoo"), d(), d(3));
