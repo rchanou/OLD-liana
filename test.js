a2bc1679713c9ae651840c610d5d655ae68f3a37
RC <rchanou@gmail.com>
2018-01-18 17:06:07 -0800

    chore: test

---
+++ test.js
@@ -0,0 +1,19 @@
+const { types } = require("mobx-state-tree");
+
+const B = types.model({
+  id: types.identifier(types.number),
+  name: types.string,
+  ref: types.reference(types.late(() => B))
+});
+
+const A = types.model({
+  b: B,
+  ref: types.reference(B)
+});
+
+const a = A.create({
+  b: { id: 1, name: "rsttars", ref: 1 },
+  ref: 1
+});
+
+console.log(a.b.ref.name);
